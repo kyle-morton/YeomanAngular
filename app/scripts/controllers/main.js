2318316fb78f439c28270d3c335a28a0f7293929
kylemorton5770 <michael.morton@smail.astate.edu>
2016-02-23 08:18:03 -0600

    added todo form

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,15 @@
  */
 angular.module('mytodoApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
+    $scope.todo = "";
+    
+    $scope.addTodo = function(){
+      console.log("ScopeTodo: " + $scope.todo != null);
+      console.log("In array: " + $.inArray($scope.todo, $scope.todos));
+      if ($scope.todo != null && ($.inArray($scope.todo, $scope.todos) == -1)){
+        $scope.todos.push($scope.todo);
+        $scope.todo = '';
+      }
+    }
   });
